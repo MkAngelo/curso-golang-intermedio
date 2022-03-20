b5df7c2e5262c846cadb97a8214c5660eed1bf77
mkangelo <mikequi000@gmail.com>
2022-03-20 13:47:39 -0600

    Interfaces y Polimorfismo

--- herencia.go
+++ herencia.go
@@ -16,10 +16,35 @@ type FullTimeEmployee struct {
 	Employee
 }
 
+func (ftEmployee FullTimeEmployee) getMessage() string {
+	return "Full Time Employee"
+}
+
+type TemporaryEmployee struct {
+	Person
+	Employee
+	taxRate int
+}
+
+func (tEmployee TemporaryEmployee) getMessage() string {
+	return "Temporary Time Employee"
+}
+
+type PrintInfo interface {
+	getMessage() string
+}
+
+func getMessage(p PrintInfo) {
+	fmt.Println(p.getMessage())
+}
+
 func main() {
 	ftEmployee := FullTimeEmployee{}
 	ftEmployee.name = "Name"
 	ftEmployee.id = 1
 	ftEmployee.age = 20
 	fmt.Printf("%v\n", ftEmployee)
+	tEmployee := TemporaryEmployee{}
+	getMessage(tEmployee)
+	getMessage(ftEmployee)
 }
